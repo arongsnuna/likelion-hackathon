abf760f4a1d97d4b58ee59ccf3386b8270bcf038
guitarcode <choicco89@gmail.com>
2022-08-20 00:48:41 +0900

    최종수정

--- festival/urls.py
+++ festival/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.festival_list, name="festival_list"),
     path("<int:pk>", views.festival_detail, name="festival_detail"),
-    path("/search", views.festival_search, name="festival_search"),
+    path("search/", views.festival_search, name="festival_search"),
 ]
