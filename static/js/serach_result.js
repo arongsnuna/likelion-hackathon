0b3dc848b9477e4e3cc35c9ee29a457bcdc8faf1
seoyon <lsy021128@naver.com>
2022-08-19 23:56:24 +0900

    검색결과창

---
+++ static/js/serach_result.js
@@ -0,0 +1,9 @@
+function showResult(){
+    var selectValue = $('#selectValue').val();
+    var inputValue = $('.form-control').val();
+    var url = 'http://127.0.0.1:8000/' + selectValue + '/search/?q=' + inputValue;
+    window.location.href = url;
+
+
+}
+
